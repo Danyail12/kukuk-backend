9012d5be05fbf2700beb9e8b978b3aa7a7c2a1a1
Danyail Abbas <141053200+Danyail12@users.noreply.github.com>
2024-01-27 00:06:11 +0500

    Delete routes/forgetPassword.js

--- routes/forgetPassword.js
+++
@@ -1,8 +0,0 @@
-import  express from "express";
-import { forgetPassword } from "../controllers/forgetPassword.js";
-
-const router = express.Router();
-
-router.route(":id/password").put(forgetPassword);
-
-export default router;
